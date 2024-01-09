e48549e26ac35386addd3751675b52f4294b0f29
Blessing Tembo <temboblessing05@gmail.com>
2024-01-09 21:39:19 +0200

    Verdion 3

--- change.js
+++ change.js
@@ -1,2 +1,4 @@
 console.log('the change has been made')
 console.log('hello world')
+
+console.log('version 3')
--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 console.log('verdion1')
 console.log('config');
 console.log('version 2')
+
+console.log('version 3')
